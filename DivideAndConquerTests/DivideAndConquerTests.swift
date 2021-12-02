b574955c5d8ff60463f7a65859fb28196d6f26a8
Jacob Bickle <bickle.jake@gmail.com>
2021-12-02 11:00:12 -0800

    Rename project files from Rectangle to DivideAndConquer

--- DivideAndConquerTests/DivideAndConquerTests.swift
+++ DivideAndConquerTests/DivideAndConquerTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import Rectangle
+@testable import DivideAndConquer
 
-class RectangleTests: XCTestCase {
+class DivideAndConquer: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
